4a4f095fd53b4a109dab43bebdc6df354f737486
zzhan729 <zzhan729@uwo.ca>
2022-02-22 03:54:01 -0500

    adding fake data and product commponent

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
+import React from "react";
+import Products from "./components/Products";
+import data from "./data.json";
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/"> React Shopping cart</a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        All right is reserved.
-      </footer>
-    </div>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    }
+  }
 
-  );
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/"> React Shopping cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}></Products>
+            </div>
+            <div className="sidebar">
+              Cart Items
+            </div>
+          </div>
+        </main>
+        <footer>
+          All right is reserved.
+        </footer>
+      </div>
+
+    );
+
+  }
+
 }
 
 export default App;
